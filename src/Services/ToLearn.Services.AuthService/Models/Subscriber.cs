b813784e4e9a643ec0370fd7c65675ce858f494f
S0dlive <sandlive662@gmail.com>
2023-02-26 21:41:06 +0100

    feat(auth-microservice): subscriber features and more.

---
+++ src/Services/ToLearn.Services.AuthService/Models/Subscriber.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToLearn.Services.AuthService.Models;
+
+public class Subscriber
+{
+    [Key]
+    public string Id { get; set; }
+    public string ViseId { get; set; }
+    public string FollowersId { get; set; }
+}
--- src/Services/ToLearn.Services.AuthService/Program.cs
+++ src/Services/ToLearn.Services.AuthService/Program.cs
@@ -1,12 +1,12 @@
+using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using ToLearn.Services.AuthService.Data;
 using ToLearn.Services.AuthService.Models;
 using ToLearn.Services.AuthService.Service;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
 
 builder.Services.AddControllers();
 
@@ -18,13 +18,26 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     string connection = "server=localhost;user=root;password=;database=tolearndb;";
     options.UseMySql(connection, ServerVersion.AutoDetect(connection));
 });
+
 builder.Services.AddSingleton<UserProfileService>();
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication().AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JwtAudience:"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
